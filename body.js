3a67a924cf039fd4b4200c37640c739008394792
e-diamond <68610675+e-diamond@users.noreply.github.com>
2022-04-13 19:10:30 +0100

    Moving acceleration calculation to separate method

--- body.js
+++ body.js
@@ -16,13 +16,23 @@ class Body {
     pop();
   }
 
-  update(bodies, solve) {
+  update(bodies, solver) {
     // get total acceleration
-    var acc = createVector(0, 0, 0);
+    let a = this.calcAcceleration(bodies);
 
+    // perform integration
+    // timestep
+    let dt = 0.01;
+    // update position vector
+    this.r = solver(this.r, this.v, dt);
+    // update velocity vector
+    this.v = solver(this.v, a, dt);
+  }
+
+  calcAcceleration(bodies) {
+    var acc = createVector(0, 0, 0);
     // compare this to all other bodies
     for (var body of bodies) {
-
       let diff = vector.sub(body.r, this.r);
       let magsq = diff.magSq();
       // ignore if bodies are at same position (are same body)
@@ -31,13 +41,6 @@ class Body {
         acc.add(vector.mult(diff, body.m/(Math.pow(magsq, 1.5))));
       }
     }
-
-    // perform integration  
-    // timestep
-    let dt = 0.01;
-    // update position vector
-    this.r = solve(this.r, this.v, dt);
-    // update velocity vector
-    this.v = solve(this.v, acc, dt);
+    return acc;
   }
 }
